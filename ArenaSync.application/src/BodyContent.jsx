0a91b48e08af514f997fe3b292c8970bd0f68283
abderrahim177 <abdorrahimakarchach@gmail.com>
2026-03-12 15:14:47 +0000

    ajouter style des cards

--- ArenaSync.application/src/BodyContent.jsx
+++ ArenaSync.application/src/BodyContent.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { tournamentData } from "./tournamentDB";
-
 export default function AffichDta() {
-
   const [status, setstatus] = useState("");
   const [name, setname] = useState("");
   const [selectedTournament, setSelectedTournament] = useState(null);
@@ -32,21 +30,16 @@ export default function AffichDta() {
     filter === "All"
       ? tournamentData
       : tournamentData.filter((el) => el.status === filter);
-
   return (
     <div className="Father">
-
       <div>
-
         <div className="buttons">
           <button onClick={() => setFilter("All")}>All</button>
           <button onClick={() => setFilter("On_Going")}>On Going</button>
           <button onClick={() => setFilter("Upcoming")}>Upcoming</button>
           <button onClick={() => setFilter("Pending")}>Pending</button>
         </div>
-
         <hr className="hr" />
-
         <div className="content">
           {filteredData.map((element) => (
             <div
@@ -59,38 +52,29 @@ export default function AffichDta() {
                   <div>
                     <img className="image" src={element.img} alt="" />
                   </div>
-
-                  <div>
+                  <div >
                     <h2 className="title">{element.title}</h2>
-
                     <button className={`status ${element.status}`}>
                       {element.status}
                     </button>
                   </div>
                 </div>
-
                 <p>{element.description}</p>
-
                 <hr />
-
-                <p>
+                <p className="count">
                   <i className="fa-solid fa-users"></i>{" "}
                   {element.participantsCount} <span>{element.type}</span>
                 </p>
               </div>
-
               <p>
                 <i className="fa-solid fa-trophy"></i> {element.location}
               </p>
-
               <p>
                 <i className="fa-solid fa-calendar"></i> {element.date}
               </p>
-
               <p>
                 <i className="fa-solid fa-location-dot"></i> {element.format}
               </p>
-
               <div className="AddingBtn">
                 <button
                   className="btn"
@@ -106,11 +90,9 @@ export default function AffichDta() {
             </div>
           ))}
         </div>
-
         {Form && (
           <div className="overlay">
             <div className="containere">
-
               <input
                 type="file"
                 accept="image/*"
@@ -121,7 +103,6 @@ export default function AffichDta() {
                   }
                 }}
               />
-
               {image && (
                 <img
                   src={image}
@@ -131,7 +112,6 @@ export default function AffichDta() {
                   style={{ borderRadius: "50%" }}
                 />
               )}
-
               <input
                 className="name"
                 type="text"
@@ -139,7 +119,6 @@ export default function AffichDta() {
                 value={name}
                 onChange={(e) => setname(e.target.value)}
               />
-
               <input
                 className="Status"
                 type="text"
@@ -147,57 +126,42 @@ export default function AffichDta() {
                 value={status}
                 onChange={(e) => setstatus(e.target.value)}
               />
-
               <div className="Buttons">
                 <button className="Save" onClick={AddParticpant}>
                   Save
                 </button>
-
                 <button className="Back" onClick={() => setform(false)}>
                   Back
                 </button>
               </div>
-
             </div>
           </div>
         )}
       </div>
-
       <div className="details">
-
         <div>
           <button>Participant</button>
         </div>
-
         <hr />
-
         {selectedTournament && (
           <div>
-
             <div className="title_Card">
               <h2>{selectedTournament.title}</h2>
             </div>
-
             <div className="Participants">
               {selectedTournament.participants.map((p) => (
                 <div className="part" key={p.id}>
-
                   <img className="images" src={p.avatar} alt="" />
-
                   <div>
                     <span className="name">{p.name}</span>
                     <p className={`status ${p.status}`}>{p.status}</p>
                   </div>
-
                 </div>
               ))}
             </div>
-
           </div>
         )}
-
       </div>
-
     </div>
   );
 }
